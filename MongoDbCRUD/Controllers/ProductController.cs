ad1f0769d9100681ca3450c501d4223c57c23e5a
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2019-02-05 12:25:13 +0530

    crudOp

---
+++ MongoDbCRUD/Controllers/ProductController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver.Core;
+using System.Configuration;
+using MongoDbCRUD.App_Start;
+using MongoDB.Driver;
+using MongoDbCRUD.Models;
+
+namespace MongoDbCRUD.Controllers
+{
+    public class ProductController : Controller
+    {
+        private MongoDBContext dbcontext;
+        private IMongoCollection<ProductModel> productCollection;
+
+        public ProductController()
+        {
+            dbcontext = new MongoDBContext();
+            productCollection = dbcontext.database.GetCollection<ProductModel>("product");
+        }
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<ProductModel> products = productCollection.AsQueryable<ProductModel>().ToList();
+            return View(products);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(string id)
+        {
+            var productId = new ObjectId(id);
+            var product = productCollection.AsQueryable<ProductModel>().SingleOrDefault(x=>x.Id==productId);
+            return View(product);
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(ProductModel product)
+        {
+            try
+            {
+                //  Db is updated from here boiyee
+                productCollection.InsertOne(product);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(string id)
+        {
+            var productId = new ObjectId(id);
+            var product = productCollection.AsQueryable<ProductModel>().SingleOrDefault(x => x.Id == productId);
+
+            return View(product);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(string id, ProductModel product)
+        {
+            try
+            {
+                 var filter = Builders<ProductModel>.Filter.Eq("_id", ObjectId.Parse(id));
+                 var update = Builders<ProductModel>.Update.Set("ProductName", product.ProductName)
+                    .Set("ProductDescription", product.ProductDescription)
+                    .Set("Quantity", product.Quantity);
+
+                var result = productCollection.UpdateOne(filter, update);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(string id)
+        {
+            var productId = new ObjectId(id);
+            var product = productCollection.AsQueryable<ProductModel>().SingleOrDefault(x => x.Id == productId);
+
+            return View(product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(string id, FormCollection collection)
+        {
+            try
+            {
+                productCollection.DeleteOne(Builders<ProductModel>.Filter.Eq("_id", ObjectId.Parse(id)));
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MongoDbCRUD/Models/ProductModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDbCRUD.Models
+{
+    public class ProductModel
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("ProductName")]
+        public string ProductName { get; set; }
+
+        [BsonElement("ProductDescription")]
+        public string ProductDescription { get; set; }
+
+        [BsonElement("Quantity")]
+        public string Quantity { get; set; }
+
+    }
+}
